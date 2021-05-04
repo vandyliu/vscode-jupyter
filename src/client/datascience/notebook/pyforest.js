c244b0c45f0232c7eba30a5101adad3c2a8e40bc
Ian Huff <ianhuff@Ians-MacBook-Pro.local>
2021-05-04 13:03:02 -0700

    pyforest

--- src/client/datascience/notebook/pyforest.js
+++ src/client/datascience/notebook/pyforest.js
@@ -1,4 +1,7 @@
-console.log('IANHU test message');
-alert('testing');
+console.log('Pyforest intercept script running');
 const vscode = acquireVsCodeApi();
 vscode.postMessage({message: 'testing'});
+
+// window._pyforest_update_imports_cell = function (imports_string) {
+    // aznb.postMessage({command: "autoImport", imports_string});
+  // };
