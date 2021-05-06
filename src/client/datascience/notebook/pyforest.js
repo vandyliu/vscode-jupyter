96c34d870187cb8643e4cc7d467bf72d447634cb
Ian Huff <ianhuff@Ians-MacBook-Pro.local>
2021-05-06 16:17:27 -0700

    update to new message API

--- src/client/datascience/notebook/pyforest.js
+++ src/client/datascience/notebook/pyforest.js
@@ -1,6 +1,7 @@
 console.log('Pyforest intercept script running');
-const vscode = acquireVsCodeApi();
+//const vscode = acquireVsCodeApi();
 
 window._pyforest_update_imports_cell = function (imports_string) {
-  vscode.postMessage({importString: imports_string});
+  //vscode.postMessage({importString: imports_string});
+  postKernelMessage({importString: imports_string});
 }
